80d5319eddd1ab737b5b541a902f23b2e0ea6a2d
Alpay KOÇAK <alpaykocak@hotmail.com>
2021-05-13 14:20:54 +0300

    Extended with noe types of emations

--- app.R
+++ app.R
@@ -12,7 +12,7 @@ library(proto)
 library(lubridate)
 library(ggplot2)
 library(tidyr)
-alpay <- readRDS("alpay.RDS") ## This is the dictionary file
+alpay <- readRDS("alpay.RDS") ## This is the dictionary file filtered positive negative
 duygu <- function(dosya) {
     text <- pdf_text(pdf = dosya)
     text <- paste(text,collapse = " ")
@@ -31,38 +31,56 @@ duygu <- function(dosya) {
         emotion_by(emotion_dt = alpay)
     x <- emotion  %>%
         select(emotion_type, ave_emotion) %>%
-        filter (str_detect(emotion_type, "negated"))
+        filter (str_detect(emotion_type, "negated")) %>% arrange(as.character(emotion_type))
     y <- emotion  %>%
         select(emotion_type, ave_emotion) %>%
         filter (!str_detect(emotion_type, "negated"))
     emotions <- data.frame("emotion_type" = y$emotion_type,
                            "ave_emotion" = y[,2]-x[,2])
-    rates <- data.frame("min" = rep(0,2),"max" = c(0.2309603,0.3316599),"mean" = c(0.04689535,0.07893328))
-    negativitiy_index <- scales::rescale_mid(emotions$ave_emotion[1],
-                                             from = c(0,rates$max[1]),
-                                             to = c(0,100),
-                                             mid = rates$mean[1])
+    rates <<- data.frame("emotion_type" = y$emotion_type,
+                        "min" = rep(0,7),
+                        "max" = c(0.3,0.3,0.2309603,0.3316599,0.3,0.3,0.3),
+                        "mean" = c(0.02,0.02,0.04689535,0.07893328,0.01,0.02,0.01))
+    index_fun <- function(x) {
+        ind <- scales::rescale_mid(emotions[emotions$emotion_type == as.character(x),"ave_emotion"],
+                                   from = c(rates[rates$emotion_type == as.character(x),"min"],
+                                            rates[rates$emotion_type == as.character(x),"max"]),
+                                   to = c(0,100),
+                                   mid = rates[rates$emotion_type == as.character(x),"mean"])
+        return(ind)
+    }
     
-    positivity_index <- scales::rescale_mid(emotions$ave_emotion[2],
-                                            from = c(0,rates$max[2]),
-                                            to = c(0,100),
-                                            mid = rates$mean[2])
-    balance_index <- scales::rescale_mid((emotions$ave_emotion[2]-emotions$ave_emotion[1]),
-                                         from = c(-mean(rates$max[1],rates$max[2]),mean(rates$max[1],rates$max[2])),
-                                         to = c(0,100),
-                                         mid = 0)
     results <- data.frame("Document Name" = basename(dosya),
                           "Total Word Count" = emotion$word_count[1],
+                          "Total anticipation Word" = emotion[emotion$emotion_type == "anticipation","emotion_count"][[1]],
+                          "Total anticipation Negated Word" = emotion[emotion$emotion_type == "anticipation_negated","emotion_count"][[1]],
+                          "Total fear Word" = emotion[emotion$emotion_type == "fear","emotion_count"][[1]],
+                          "Total fear Negated Word" = emotion[emotion$emotion_type == "fear_negated","emotion_count"][[1]],
                           "Total Negative Word" = emotion[emotion$emotion_type == "negative","emotion_count"][[1]],
                           "Total Negative Negated Word" = emotion[emotion$emotion_type == "negative_negated","emotion_count"][[1]],
                           "Total Positive Word" = emotion[emotion$emotion_type == "positive","emotion_count"][[1]],
                           "Total Positive Negated Word" = emotion[emotion$emotion_type == "positive_negated","emotion_count"][[1]],
-                          "Raw Negative Ratio"  =emotions$ave_emotion[1],
-                          "Raw Positive Ratio"  =emotions$ave_emotion[2],
-                          "Negativity Index" = negativitiy_index,
-                          "Positivity Index" = positivity_index,
-                          "Balance Index" = balance_index)
-    rm(emotions,emotion,rates,x,y,negativitiy_index,positivity_index,text,dosya)
+                          "Total surprise Word" = emotion[emotion$emotion_type == "surprise","emotion_count"][[1]],
+                          "Total surprise Negated Word" = emotion[emotion$emotion_type == "surprise_negated","emotion_count"][[1]],
+                          "Total trust Word" = emotion[emotion$emotion_type == "trust","emotion_count"][[1]],
+                          "Total trust Negated Word" = emotion[emotion$emotion_type == "trust_negated","emotion_count"][[1]],
+                          "Total uncertainty Word" = emotion[emotion$emotion_type == "uncertainty","emotion_count"][[1]],
+                          "Total uncertainty Negated Word" = emotion[emotion$emotion_type == "uncertainty_negated","emotion_count"][[1]],
+                          "Raw anticipation Ratio"  =emotions$ave_emotion[1],
+                          "Raw fear Ratio"  =emotions$ave_emotion[2],
+                          "Raw Negative Ratio"  =emotions$ave_emotion[3],
+                          "Raw Positive Ratio"  =emotions$ave_emotion[4],
+                          "Raw surprise Ratio"  =emotions$ave_emotion[5],
+                          "Raw trust Ratio"  =emotions$ave_emotion[6],
+                          "Raw uncertainty Ratio"  =emotions$ave_emotion[7],
+                          "anticipation Index" = index_fun(y$emotion_type[1]),
+                          "fear Index" = index_fun(y$emotion_type[2]),
+                          "Negativity Index" = index_fun(y$emotion_type[3]),
+                          "Positivity Index" = index_fun(y$emotion_type[4]),
+                          "surprise Index" = index_fun(y$emotion_type[5]),
+                          "trust Index" = index_fun(y$emotion_type[6]),
+                          "uncertainty Index" = index_fun(y$emotion_type[7]))
+    rm(emotions,emotion,x,y,text,dosya)
     return(results)
 }
 fill_NA <- function(x) {
@@ -128,11 +146,7 @@ server <- function(input, output,session) {
             if (is.null(inFile)) {
                 return(NULL)
             } else {
-                # d <- duygu(inFile$datapath)
-                rates <- data.frame("Emotions" = c("Negative","Positive"),
-                                    "Historical raw min" = rep(0,2),
-                                    "Historical raw max" = c(0.2309603,0.3316599),
-                                    "Historical raw mean" = c(0.04689535,0.07893328))
+                # d <- duygu(inFile$datapath))
                 d <- as.data.frame(t(sapply(inFile$datapath, duygu)))
                 d[,1] <- sub(".pdf$", "", basename(inFile$name))
                 
@@ -151,7 +165,7 @@ server <- function(input, output,session) {
                 d2 <- as.data.frame(sapply(d, unlist))
                 d2$Document.Name.Date.Estimated <- as.Date(as.numeric(d2$Document.Name.Date.Estimated),origin = "1970-01-01")
                 d2 <- d2 %>% right_join(DATE,by = "Document.Name.Date.Estimated") %>% arrange(Document.Name.Date.Estimated)
-                d3 <- bind_cols(as.data.frame(sapply(d2[,c(3:12)], fill_NA)),
+                d3 <- bind_cols(as.data.frame(sapply(d2[,c(3:dim(d2)[2])], fill_NA)),
                                  "Document.Name.Date.Estimated"= d2$Document.Name.Date.Estimated,
                                  "Document.Name" = d2$Document.Name)
                 d4 <- d3 %>% mutate(YEAR = year(Document.Name.Date.Estimated), 
@@ -159,7 +173,12 @@ server <- function(input, output,session) {
                     dplyr::select(-Document.Name.Date.Estimated,-Document.Name) %>% group_by(YEAR, MONTH) %>%
                     summarise_all(max)
                 d4 <- d4  %>%
-                    dplyr::select(YEAR,MONTH,Negativity.Index,Positivity.Index,Balance.Index)
+                    dplyr::select(YEAR,MONTH, anticipation.Index, fear.Index,
+                                  Negativity.Index,
+                                  Positivity.Index,
+                                  surprise.Index,
+                                  trust.Index,
+                                  uncertainty.Index)
                 },  error = function(e) d4 <<- NULL)} else {d4 <- NULL}
                 
             }
@@ -185,11 +204,20 @@ server <- function(input, output,session) {
                     
                     class = "display"
                 ) %>%
-                    formatRound(columns=c("Raw.Negative.Ratio", 
+                    formatRound(columns=c("Raw.anticipation.Ratio",
+                                          "Raw.fear.Ratio",
+                                          "Raw.Negative.Ratio",
                                           "Raw.Positive.Ratio",
+                                          "Raw.surprise.Ratio",
+                                          "Raw.trust.Ratio",
+                                          "Raw.uncertainty.Ratio",
+                                          "anticipation.Index",
+                                          "fear.Index",
                                           "Negativity.Index",
                                           "Positivity.Index",
-                                          "Balance.Index"), digits=3)
+                                          "surprise.Index",
+                                          "trust.Index",
+                                          "uncertainty.Index"), digits=3)
             })
            
            output$sonuc2 <- 
@@ -216,9 +244,13 @@ server <- function(input, output,session) {
                    class = "display"
                ) %>%
                    formatRound(columns=c(
-                                         "Negativity.Index",
-                                         "Positivity.Index",
-                                         "Balance.Index"), digits=3)
+                       "anticipation.Index",
+                       "fear.Index",
+                       "Negativity.Index",
+                       "Positivity.Index",
+                       "surprise.Index",
+                       "trust.Index",
+                       "uncertainty.Index"), digits=3)
            })
                }
            
@@ -234,7 +266,7 @@ server <- function(input, output,session) {
                     mutate(Date = ymd(paste0(YEAR,"-",MONTH,"-","01"))) %>% 
                     select(-YEAR,-MONTH) %>% gather("type", "data",-Date)
                 ggplot(d5,aes(Date,data)) + geom_line() + facet_grid(type~.) +
-                    labs(x="Document Time", y="Balance Index")
+                    labs(x="Document Time", y="uncertainty.Index")
                 })} else {NULL}
     })
     
